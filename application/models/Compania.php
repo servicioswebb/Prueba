d635977e2657a117abd94d4566f10ff39ec42983
edwynr <edu88_1@hotmail.com>
2016-10-13 16:15:36 -0400

    creando el modelo 1

---
+++ application/models/Compania.php
@@ -0,0 +1,17 @@
+<?php
+
+
+	class Compania extends CI_Model{  //Creando el controlador para la clase compañia
+		
+		public function __construct()
+		{
+			$this->load->database();
+						
+		}	
+		public function index ()
+		{
+			
+		}
+		
+		
+	}
