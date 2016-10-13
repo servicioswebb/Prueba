68c52218be8a467a5cf4eca5db77b5a633be9447
eromero <edu88_1@hotmail.com>
2016-10-13 19:15:41 -0400

    creando los controladores

---
+++ application/views/bienvenido.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pagina de bienvenida del sistema</title>
+</head>
+<body>
+<h1><?php echo $tutoria; ?></h1>
+</body>
+</html>
