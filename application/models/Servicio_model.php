b3ff2a86f2d9364857d89e3e5752b655d1112eec
edwynr <edu88_1@hotmail.com>
2016-10-20 14:03:50 -0400

    Actualizacion 20 octubre

---
+++ application/models/Servicio_model.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Compania_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+
+    }
+
+    public function getCompania()
+    {
+        return $this->db->get('compania');
+
+    }
+    public function crearCompania($data)
+    {
+        $this->db->insert('compania', array('Nombre compañia' => $data['nombre'], 'actividad compañia' => $data['actividad']));
+    }
+
+    public function getCompaniaById($id = '')
+    {
+        $result = $this->db->query(" SELECT * FROM compania WHERE idCompa='" . $id . "' LIMIT 1");
+        return $result->row();
+    }
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,1 +1,13 @@
-cx
+<?php
+
+/**
+ *
+ */
+class LoginController extends CI_Controller
+{
+
+    public function gerUser()
+    {
+	$result = $this->db->query(" SELECT * FROM usuario WHERE nomUsu='" . $nUsu . "' and claUsu='" . $claUsu . "' LIMIT 1");
+    }
+}
