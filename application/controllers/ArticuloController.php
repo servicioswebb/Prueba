c5fe68a0b6e22f239d29ef5d88f7db80eb2bf8f6
edwynr <edu88_1@hotmail.com>
2016-10-20 13:08:43 -0400

    subiendo la vista individual de compañia

--- application/controllers/ArticuloController.php
+++ application/controllers/ArticuloController.php
@@ -6,9 +6,23 @@ class ArticuloController extends CI_Controller
 {
     public function compania($id = '')
     {
+			
         $fila = $this->Compania_model->getCompaniaById($id);
-        echo $fila->idCompa;
-        echo $fila->nomCompa;
-        echo $fila->actividadCompa;
+		$this->load->view('layouts/head');
+        $this->load->view('layouts/header');
+        $this->load->view('layouts/main');
+        //$this->load->model('Compania_model');
+        
+		$data = array(
+		'nomCompa' => $fila->nomCompa,
+		'actividadCompa' => $fila->actividadCompa
+		);
+		
+		$data      = array('contenido' => $fila->idCompa);
+		$data      = array('contenido' => $fila->nomCompa);
+		$data      = array('contenido' => $fila->actividadCompa);
+        $this->load->view('compania/content1', $data);
+        $this->load->view('layouts/footer');
+
     }
 }
---
+++ application/views/compania/content1.php
@@ -0,0 +1,12 @@
+
+    <!-- Post Content -->
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <p><?= $contenido ?></p>
+
+                </div>
+            </div>
+        </div>
+    </article>
